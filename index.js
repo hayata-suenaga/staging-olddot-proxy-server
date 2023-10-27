618a2171197e5c4d8a4add5e8966ec7a1f15a3a1
Hayata Suenaga <hayata@expensify.com>
2023-10-27 09:27:22 -0700

    chore: temporarily modify the proxy code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
-const target = "https://expensify.com.dev";
+const target = "https://staging.expensify.com";
 
 // Proxy middleware options.
 const options = {
@@ -11,7 +11,7 @@ const options = {
   ws: true, // proxy websockets
   router: {
     // when request.headers.host == 'new.expensify.com.dev', proxy to:
-    "new.expensify.com.dev": target,
+    localhost: target,
   },
 };
 
@@ -24,6 +24,6 @@ app.use(apiProxy);
 // Serve static files or other routes here if needed
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Proxy server is running on http://localhost:3000");
+app.listen(4000, () => {
+  console.log("Proxy server is running on new.expensify.com.dev");
 });
